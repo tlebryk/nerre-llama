9cc1779cbaa57a5e623270f8bf8f74119b33794e
tlebryk <tlebryk02@gmail.com>
2024-11-30 00:40:29 -0500

    refactor to put in a subdir

---
+++ nerre_llama/ft_datasets/__init__.py
@@ -0,0 +1,13 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# This software may be used and distributed according to the terms of the Llama 2 Community License Agreement.
+
+from .grammar_dataset import get_dataset as get_grammar_dataset
+from .alpaca_dataset import InstructionDataset as get_alpaca_dataset
+from .samsum_dataset import get_preprocessed_samsum as get_samsum_dataset
+#from .custom_dataset import DopedMatDatasetEngextra as get_dopingengextra_dataset
+#from .custom_dataset import DopedMatDatasetJson as get_dopingjson_dataset
+from .custom_dataset import DopedMatDataset as get_doping_dataset
+from .custom_dataset import GenMatDataset as get_genmat_dataset
+from .custom_dataset import MOFDataset as get_mof_dataset
+
+from .custom_dataset import AuNRDataset as get_AuNR_dataset
